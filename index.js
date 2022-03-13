c95a7f9d53cf3291abd070066b730773673fbca6
sajandhakal12 <sajan12dhakal@gmail.com>
2022-03-13 20:13:23 +0545

    changes

--- index.js
+++ index.js
@@ -89,3 +89,33 @@ io.on("connection", (socket) => {
 server.listen(process.env.port || 4000, () => {
   console.log(`Listening to port ${process.env.port || 4000}`);
 });
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
+});
+
+process.once("SIGHUP", function () {
+  process.kill(process.pid, "SIGHUP");
+});
