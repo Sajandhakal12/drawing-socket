2e68e6f4a9a7fc740cd9ae1b174fc7861a5a0176
sajandhakal12 <sajan12dhakal@gmail.com>
2022-03-13 20:02:54 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const socketio = require("socket.io");
+const { createServer } = require("http");
+const express = require("express");
+const cors = require("cors");
+
+const allowedList = require("./allowedList");
+
+const app = express();
+
+const server = createServer(app);
+
+const users = {};
+const drawing = {};
+const sockets = {};
+
+const io = socketio(server, {
+  cors: {
+    origin: (origin, fn) => {
+      if (allowedList.indexOf(origin) !== -1) return fn(null, origin);
+      return fn("Error Invalid domain");
+    },
+    methods: ["GET", "POST"],
+  },
+  //   allowEIO3: true,
+});
+
+app.use(cors());
+app.options("*", cors());
+
+io.on("connection", (socket) => {
+  socket.on("join-drawing", ({ drawingId, name, color }) => {
+    socket.join(drawingId);
+    if (!users[drawingId]) {
+      users[drawingId] = {};
+    } else {
+      users[drawingId][color] = { name, color };
+    }
+    sockets[socket.id] = { drawingId, color };
+    socket.to(drawingId).emit("joined-users", {
+      users: users[drawingId],
+    });
+    io.to(socket.id).emit("joined-drawing", {
+      users: users[drawingId],
+      drawing: drawing[drawingId] || [],
+    });
+  });
+
+  socket.on("leave-drawing", () => {
+    const canvasInfo = sockets[socket.id] || {};
+    if (canvasInfo.drawingId && canvasInfo.color) {
+      delete users[canvasInfo.drawingId][canvasInfo.color];
+      socket
+        .to(canvasInfo.drawingId)
+        .emit("left-drawing", users[canvasInfo.drawingId]);
+      socket.leave(canvasInfo.drawingId);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const canvasInfo = sockets[socket.id] || {};
+    if (canvasInfo.drawingId && canvasInfo.color) {
+      delete users[canvasInfo.drawingId][canvasInfo.color];
+      socket
+        .to(canvasInfo.drawingId)
+        .emit("left-drawing", users[canvasInfo.drawingId]);
+      socket.leave(canvasInfo.drawingId);
+    }
+  });
+
+  socket.on("input-canvas", ({ drawingId, msg }) => {
+    if (!drawing[drawingId]) {
+      drawing[drawingId] = [];
+    } else {
+      drawing[drawingId].push(msg);
+    }
+    socket.to(drawingId).emit("update-canvas", msg);
+    console.log(drawing, users);
+  });
+
+  socket.on("update-canvas", ({ drawingId, msg }) => {
+    drawing[drawingId] = msg;
+  });
+
+  socket.on("input-control", ({ drawingId, type }) => {
+    socket.to(drawingId).emit("update-control", type);
+  });
+});
+
+server.listen(process.env.port || 4000, () => {
+  console.log(`Listening to port ${process.env.port || 4000}`);
+});
