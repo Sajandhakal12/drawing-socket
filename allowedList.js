5f38437c885364f5e0d15ca9f0d357a0b320cc2b
sajandhakal12 <sajan12dhakal@gmail.com>
2022-03-13 20:20:01 +0545

    url updated

--- allowedList.js
+++ allowedList.js
@@ -1,5 +1,5 @@
 module.exports = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://interactive-drawing-app-ahmpt7uec-sajandhakal12.vercel.app/",
+  "https://interactive-drawing-app.vercel.app/",
 ];
